331a706435f7a95d7dab2b6e77ac4908413f929b
frank-feulner <frank.feulner@aexea.de>
2015-06-23 22:58:11 +0200

    Fixed shit, broke shit
    Tried to add error for params bigger than 3.
    Breaks now

--- HelloWorldParams.java
+++ HelloWorldParams.java
@@ -1,26 +1,41 @@
 public class HelloWorldParams {
   String myString;
+
   public static String[] Responses;
-  void MakeResponsesArray()
+  static void MakeResponsesArray()
   {
-    Responses = new String[5];
+    Responses = new String[4];
   }
-  public void FillResponsesArray()
+
+  public static void FillResponsesArray()
   {
+    Responses[0] = "No";
     Responses[1] = "One";
     Responses[2] = "Two";
     Responses[3] = "Three";
   }
-  public static String MakeMyString(String[] args)
+
+  public static String MakeMyString(int myParam, String[] myArr)
   {
-    int tmpIndex;
-    try {tmpIndex = Integer.parseInt(args[0]);}
-    catch (NumberFormatException ex) {tmpIndex = 0;}
-    String tmpString = "Hello " + Responses[tmpIndex] + " World";
+    if myParam < Responses.length {
+    String tmpInputString = myArr[myParam];
+    String tmpString = "Hello " + tmpInputString + " World";
     return tmpString;
+    }
   }
+
   public static void main(String[] args) {
-    String myString = MakeMyString(args);
+    int firstArg = 0;
+    if (args.length > 0) {
+      try {
+          firstArg = Integer.parseInt(args[0]);
+      } catch (NumberFormatException e) {
+          firstArg = 1;
+      }
+    }
+    MakeResponsesArray();
+    FillResponsesArray();
+    String myString = MakeMyString(firstArg, Responses);
     System.out.println(myString);
   }
 }
