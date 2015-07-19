3b83af3256c0738962bcd96a2e1091454c439ad2
frank-feulner <frank.feulner@aexea.de>
2015-07-19 11:16:27 +0200

    Cleanup
    Added comments and generalized the function for plural inflection.

--- HelloWorldParams.java
+++ HelloWorldParams.java
@@ -1,12 +1,16 @@
 public class HelloWorldParams {
   String myString;
 
+//Dieser Teil deklariert meinen Array
   public static String[] Responses;
+
+//Dieser Teil instanziert meinen Array
   static void MakeResponsesArray()
   {
     Responses = new String[4];
   }
 
+//Dieser Teil füllt meinen Array
   public static void FillResponsesArray()
   {
     Responses[0] = "No";
@@ -15,17 +19,35 @@ public class HelloWorldParams {
     Responses[3] = "Three";
   }
 
+//Dieser Teil definiert, instanziert und füllt meinen String mit dem Suffix "s"
+  public static String Suffix = "s";
+
+/*Dieser Teil fügt meinen Ausgabestring zusammen.
+  Die Funktion akzeptiert zwei Parameter. Eine Ganzzahl und einen Array vom Typ String.
+  Sie fragt ab, ob die Ganzzahl kleiner als zwei ist. Falls nicht, wird das Suffix zur Bildung des Ausgabestrings verwendet.
+*/
   public static String MakeMyString(int myParam, String[] myArr)
   {
     int myArrayLength = Responses.length;
-    if (myParam < myArrayLength) {
-    String tmpInputString = myArr[myParam];
-    String tmpString = "Hello " + tmpInputString + " World";
-    return tmpString;
+    if (myParam < myArrayLength){
+      String tmpInputString = myArr[myParam];
+      String tmpString;
+      if (myParam < 2) {
+        tmpString = "Hello " + tmpInputString + " World";
+      }
+      else{
+        tmpString = "Hello " + tmpInputString + " World" + Suffix;
+      }
+      return tmpString;
     }
     else {return "Bye World. Y u no enter valid param. Izza 0 to 3";}
   }
 
+/*Diese Funktion wird angesprochen, wenn die Klasse HelloWorldParams ausgeführt wird.
+  Sie arbeitet mit der Konstanten args (Aufrufargumente).
+  Sie prüft, ob Args gefüllt ist.
+  Sie fängt ab, ob Args numerisch gefüllt ist.
+*/
   public static void main(String[] args) {
     int firstArg = 0;
     if (args.length > 0) {
@@ -35,9 +57,14 @@ public class HelloWorldParams {
           firstArg = 1;
       }
     }
+    //Konstruierende Funktionen aufrufen, damit der Array Responses gefüllt ist.
     MakeResponsesArray();
     FillResponsesArray();
+
+    //Funktion ausführen, die den String zusammensetzt.
     String myString = MakeMyString(firstArg, Responses);
+
+    //Konstruierten String auf die Kommandozeile zurückschreiben (Standard Out)
     System.out.println(myString);
   }
 }
